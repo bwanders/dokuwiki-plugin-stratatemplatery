a0117f2f2e0677327e85ec312d664cc72b41536a
Brend Wanders <b.wanders@utwente.nl>
2013-08-16 12:54:44 +0200

    Adds fields display.
    The visualisation of fields mimicks that of the data entry.

---
+++ syntax/fields.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * DokuWiki Plugin Strata Templatery (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Brend Wanders <b.wanders@utwente.nl>
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die('Meh.');
+
+class syntax_plugin_stratatemplatery_fields extends syntax_plugin_templatery_native {
+    function __construct() {
+        parent::__construct();
+        $this->util =& plugin_load('helper', 'strata_util');
+        $this->triples =& plugin_load('helper', 'strata_triples');
+    }
+
+    /**
+     * The name of the native template. This is used to determine the
+     * required syntax. (i.e., the syntax is @@->name@@).
+     */ 
+    protected function getName() {
+        return 'fields(?:\|.+?)?';
+    }
+
+    public function handle($match, $state, $pos, &$handler){
+        preg_match('/@@->fields(?:\|(.+?))?@@/', $match, $m);
+        return !empty($m[1]) ? explode(',', $m[1]) : array();
+    }
+   
+    public function render($mode, &$R, $data) {
+        if($this->isPreview() && $mode == 'xhtml') {
+           $R->doc .= '<span class="templatery-include">&#8594;fields';
+            if(count($data)) {
+                $fields = array();
+                foreach($data as $f) $fields[] = $R->_xmlEntities($f);
+                $R->doc .= ' <span class="value-separator">&#187;</span> ' . implode($fields, ', ');
+            }
+            $R->doc .= '</span>';
+            return true;
+        }
+
+        if(!$this->isPreview()) {
+            $R->table_open();
+            
+            // render a row for each key, displaying the values as comma-separated list
+            foreach($data as $field) {
+                if($this->hasField($field)) {
+                    // render row header
+                    $R->tablerow_open();
+                    $R->tableheader_open();
+                    $this->util->renderPredicate($mode, $R, $this->triples, $field);
+                    $R->tableheader_close();
+
+                    // render row content
+                    $R->tablecell_open();
+                    $this->displayField($mode, $R, $field, '');
+                    $R->tablecell_close();
+                    $R->tablerow_close();
+                }
+            }
+
+            $R->table_close();
+
+            return true;
+        }
+
+        return false;
+    }
+}
+
